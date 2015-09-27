2c8270d8e6c21ce5f5489df241eb947bf3fc7405
Edward Carmack <AriemethAcc@gmail.com>
2015-09-27 06:17:43 -0400

    Added enemies

--- main.go
+++ main.go
@@ -2,11 +2,68 @@
 package main
 
 import (
+	"math/rand"
+	"time"
+
 	"github.com/Ariemeth/go_game_jam/display"
 	"github.com/Ariemeth/go_game_jam/mech"
 	"github.com/Ariemeth/go_game_jam/mech/weapon"
 	tl "github.com/JoelOtter/termloop"
 )
+
+// GenerateEnemyMechs creates a slice of mechs to be used as enemies
+func GenerateEnemyMechs(number int) []*mech.Mech {
+	enemyMechs := make([]*mech.Mech, 0, number)
+
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+
+	for i := 0; i < number; i++ {
+		var m *mech.Mech
+
+		chance := i % 7
+		x := -15 + r.Intn(30)
+		y := -15 + r.Intn(30)
+
+		switch chance {
+		case 0:
+			m = mech.NewMech("Mech A", i, x, y, tl.ColorRed, rune('A'))
+			m.AddWeapon(weapon.CreateRifle())
+			break
+		case 1:
+			m = mech.NewMech("Mech B", i, x, y, tl.ColorRed, rune('B'))
+			m.AddWeapon(weapon.CreateRifle())
+			break
+		case 2:
+			m = mech.NewMech("Mech C", i, x, y, tl.ColorRed, rune('C'))
+			m.AddWeapon(weapon.CreateShotgun())
+			break
+		case 3:
+			m = mech.NewMech("Mech D", i, x, y, tl.ColorRed, rune('D'))
+			m.AddWeapon(weapon.CreateShotgun())
+			break
+		case 4:
+			m = mech.NewMech("Mech E", i, x, y, tl.ColorRed, rune('E'))
+			m.AddWeapon(weapon.CreateFist())
+			break
+		case 5:
+			m = mech.NewMech("Mech F", i, x, y, tl.ColorRed, rune('F'))
+			m.AddWeapon(weapon.CreateFist())
+			break
+		case 6:
+			m = mech.NewMech("Mech G", i, x, y, tl.ColorRed, rune('G'))
+			m.AddWeapon(weapon.CreateSword())
+			break
+		case 7:
+			m = mech.NewMech("Mech H", i, x, y, tl.ColorRed, rune('H'))
+			m.AddWeapon(weapon.CreateSword())
+			break
+		}
+
+		enemyMechs = append(enemyMechs, m)
+	}
+
+	return enemyMechs
+}
 
 func main() {
 	game := tl.NewGame()
@@ -17,6 +74,11 @@ func main() {
 		//		Ch: 'v',
 	})
 	level.AddEntity(tl.NewRectangle(10, 10, 50, 20, tl.ColorBlue))
+
+	enemies := GenerateEnemyMechs(8)
+	for _, enemy := range enemies {
+		level.AddEntity(enemy)
+	}
 
 	player := mech.NewPlayerMech("Player", 10, 1, 1, level)
 	weapon1 := weapon.CreateRifle()
