cc20841e2188ecc885ec2c2791d710e28ca06699
Edward Carmack <AriemethAcc@gmail.com>
2015-09-27 00:44:50 -0400

    player stays centered

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	level.AddEntity(tl.NewRectangle(10, 10, 50, 20, tl.ColorBlue))
 	status := tl.NewText(0, 10, "blah", tl.ColorWhite, tl.ColorBlack)
 	level.AddEntity(status)
-	player := mech.NewPlayerMech("Mech1", 2, tl.NewEntity(1, 1, 1, 1))
+	player := mech.NewPlayerMech("Mech1", 2, tl.NewEntity(1, 1, 1, 1), level)
 	level.AddEntity(player)
 	game.Screen().SetLevel(level)
 
--- mech/playerMech.go
+++ mech/playerMech.go
@@ -6,33 +6,42 @@ import (
 
 type playerMech struct {
 	mech
+	level *tl.BaseLevel
 }
 
 // NewMech is used to create a new instance of a mech with default structure.
-func NewPlayerMech(name string, maxStructure int, entity *tl.Entity) *playerMech {
-	newPlayerMech := playerMech{*NewMech(name, maxStructure, entity)}
+func NewPlayerMech(name string, maxStructure int, entity *tl.Entity, level *tl.BaseLevel) *playerMech {
+	newPlayerMech := playerMech{*NewMech(name, maxStructure, entity), level}
 	//	newPlayerMech.mech =
 	return &newPlayerMech
 }
 
 // Tick is called to process 1 tick of actions based on the
 // type of event.
-func (mech *playerMech) Tick(event tl.Event) {
+func (pMech *playerMech) Tick(event tl.Event) {
 	if event.Type == tl.EventKey { // Is it a keyboard event?
-		mech.prevX, mech.prevY = mech.entity.Position()
+		pMech.prevX, pMech.prevY = pMech.entity.Position()
 		switch event.Key { // If so, switch on the pressed key.
 		case tl.KeyArrowRight:
-			mech.entity.SetPosition(mech.prevX+1, mech.prevY)
+			pMech.entity.SetPosition(pMech.prevX+1, pMech.prevY)
 			break
 		case tl.KeyArrowLeft:
-			mech.entity.SetPosition(mech.prevX-1, mech.prevY)
+			pMech.entity.SetPosition(pMech.prevX-1, pMech.prevY)
 			break
 		case tl.KeyArrowUp:
-			mech.entity.SetPosition(mech.prevX, mech.prevY-1)
+			pMech.entity.SetPosition(pMech.prevX, pMech.prevY-1)
 			break
 		case tl.KeyArrowDown:
-			mech.entity.SetPosition(mech.prevX, mech.prevY+1)
+			pMech.entity.SetPosition(pMech.prevX, pMech.prevY+1)
 			break
 		}
 	}
+}
+
+// Draw passes the draw call to entity.
+func (pMech *playerMech) Draw(screen *tl.Screen) {
+	screenWidth, screenHeight := screen.Size()
+	x, y := pMech.entity.Position()
+	pMech.level.SetOffset(screenWidth/2-x, screenHeight/2-y)
+	pMech.entity.Draw(screen)
 }
